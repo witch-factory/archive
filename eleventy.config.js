2d06839d5336f8d2c4ba48bba98dfab6997118d9
witch-factory <soakdma37@gmail.com>
2025-07-13 18:10:21 +0900

    메타데이터 추가

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,21 @@
 /** @param {import("@11ty/eleventy/UserConfig").UserConfig} eleventyConfig */
 export default async function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("*.css");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("apple-icon.png");
+  eleventyConfig.addPassthroughCopy("witch-archive.png");
 
-	eleventyConfig.addCollection("til", (collectionApi) =>
-		collectionApi.getFilteredByGlob("data/til/*.md"),
-	);
+  eleventyConfig.addCollection("til", (collectionApi) =>
+    collectionApi.getFilteredByGlob("data/til/*.md")
+  );
 }
 
 export const config = {
-	dir: {
-		input: ".",
-		output: "_site",
-		// These are both relative to your input directory!
-		includes: "_includes",
-		layouts: "_layouts",
-	},
+  dir: {
+    input: ".",
+    output: "_site",
+    // These are both relative to your input directory!
+    includes: "_includes",
+    layouts: "_layouts",
+  },
 };
